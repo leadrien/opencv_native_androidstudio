ea5198e786deb1e5b70d456522a1bd0d7bd3127a
Ehsanul-karim <60375625+Ehsanul-karim@users.noreply.github.com>
2023-03-25 19:32:18 +0600

    sample java added from master

---
+++ app/src/test/java/sample.java
@@ -0,0 +1,24 @@
+public class sample {
+    int i,j;
+
+    public sample(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public int getJ() {
+        return j;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public void setJ(int j) {
+        this.j = j;
+    }
+}
